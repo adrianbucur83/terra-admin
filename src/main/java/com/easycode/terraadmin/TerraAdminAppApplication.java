2386dda71850fb6896ce8422cb1f04c42248d0e7
Adrian Bucur <adrian.bucur@paddypowerbetfair.com>
2021-06-13 18:39:41 +0300

    Initial commit

---
+++ src/main/java/com/easycode/terraadmin/TerraAdminAppApplication.java
@@ -0,0 +1,13 @@
+package com.easycode.terraadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraAdminAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraAdminAppApplication.class, args);
+	}
+
+}
